4a9095dc3201d65e930295794541ef247b620b82
Ajenkris Kungkung <kristt26@gmail.com>
2023-03-15 16:11:39 +0900

    first commit

---
+++ app/Controllers/Admin/Laboran.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controllers\Admin;
+
+use App\Controllers\BaseController;
+
+class Laboran extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+    public function store()
+    {
+        //
+    }
+
+    public function read($id = null)
+    {
+        //
+    }
+
+    public function post()
+    {
+        //
+    }
+
+    public function put()
+    {
+        //
+    }
+
+    public function delete($id = null)
+    {
+    }
+}
---
+++ public/js/controllers/admin.controllers.js
@@ -0,0 +1,88 @@
+angular.module('adminctrl', [])
+    // Admin
+    .controller('dashboardController', dashboardController)
+    .controller('laboranController', laboranController)
+
+    ;
+
+function dashboardController($scope, dashboardServices) {
+    $scope.$emit("SendUp", "Dashboard");
+    $scope.datas = {};
+    $scope.title = "Dashboard";
+    // dashboardServices.get().then(res=>{
+    //     $scope.datas = res;
+    // })
+}
+
+function laboranController($scope, laboranServices) {
+    $scope.$emit("SendUp", "Jenis Kamar");
+    $scope.datas = {};
+    $scope.model = {};
+    $scope.dataKamar = {};
+    laboranServices.get().then((res) => {
+        $scope.datas = res;
+        console.log(res);
+    })
+
+    $scope.setInisial = (item) => {
+        $scope.model.inisial = item.substring(0, 3).toUpperCase();
+    }
+
+    $scope.save = () => {
+        pesan.dialog('Yakin ingin?', 'Yes', 'Tidak').then(res => {
+            if ($scope.model.id) {
+                laboranServices.put($scope.model).then(res => {
+                    $scope.model = {};
+                    pesan.Success("Berhasil mengubah data");
+                })
+            } else {
+                laboranServices.post($scope.model).then(res => {
+                    $scope.model = {};
+                    pesan.Success("Berhasil menambah data");
+                })
+            }
+        })
+    }
+
+    $scope.saveFasilitas = (item) => {
+        pesan.dialog('Yakin ingin?', 'Yes', 'Tidak').then(res => {
+            if (item.id) {
+                fasilitasServices.put(item).then(res => {
+                    $scope.dataKamar = {};
+                    pesan.Success("Berhasil mengubah data");
+                })
+            } else {
+                fasilitasServices.post(item).then(res => {
+                    $scope.dataKamar.fasilitas.push(res);
+                    var id = angular.copy($scope.modell.jenis_kamar_id);
+                    $scope.modell = {};
+                    $scope.modell.jenis_kamar_id = id;
+                    pesan.Success("Berhasil menambah data");
+                })
+            }
+        })
+    }
+
+    $scope.edit = (item) => {
+        $scope.model = angular.copy(item);
+    }
+
+    $scope.setDetail = (item) => {
+        $scope.dataKamar = item;
+        $scope.modell = {};
+        $scope.modell.jenis_kamar_id = item.id;
+        console.log($scope.modell);
+    }
+
+    $scope.itemFasilitas = (item) => {
+        $scope.modell = item;
+    }
+
+    $scope.delete = (param) => {
+        pesan.dialog('Yakin ingin?', 'Ya', 'Tidak').then(res => {
+            wijkServices.deleted(param).then(res => {
+                pesan.Success("Berhasil menghapus data");
+            })
+        });
+    }
+}
